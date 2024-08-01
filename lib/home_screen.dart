0996504bef867ff6567fa3712ae387ded75bfbb7
yudhono witanto <yudhonowitanto.yw@gmail.com>
2024-08-01 11:27:12 +0700

    add infinite scroll

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,62 +12,88 @@ class MyHomeScreen extends StatefulWidget {
 }
 
 class _MyHomeScreenState extends State<MyHomeScreen> {
-  static Future<List<Pokemon>> getPokemons() async {
-    var url = Uri.parse('https://pokeapi.co/api/v2/pokemon');
-    final response =
-        await http.get(url, headers: {"Content-Type": "application/json"});
-
-    if (response.statusCode == 200) {
-      // Parse the JSON response as a map
-      final Map<String, dynamic> body = json.decode(response.body);
-
-      // Extract the 'results' list from the response
-      final List<dynamic> results = body['results'];
-
-      // Map the results to a list of Pokemon objects
-      return results.map((dynamic item) => Pokemon.fromJson(item)).toList();
-    } else {
-      // Handle error response
-      throw Exception('Failed to load Pokemons');
-    }
-  }
-
-  late Future<List<Pokemon>> pokemonsFuture;
+  static const int _limit = 20;
+  int _offset = 0;
+  bool _isLoading = false;
+  List<Pokemon> _pokemons = [];
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
-    pokemonsFuture = getPokemons();
+    _fetchPokemons();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.pixels ==
+            _scrollController.position.maxScrollExtent &&
+        !_isLoading) {
+      _fetchPokemons();
+    }
+  }
+
+  Future<void> _fetchPokemons() async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      final newPokemons = await getPokemons(_offset, _limit);
+      setState(() {
+        _offset += _limit;
+        _pokemons.addAll(newPokemons);
+      });
+    } catch (e) {
+      // Handle error
+      print(e);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  static Future<List<Pokemon>> getPokemons(int offset, int limit) async {
+    var url = Uri.parse(
+        'https://pokeapi.co/api/v2/pokemon?offset=$offset&limit=$limit');
+    final response =
+        await http.get(url, headers: {"Content-Type": "application/json"});
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> body = json.decode(response.body);
+      final List<dynamic> results = body['results'];
+      return results.map((dynamic item) => Pokemon.fromJson(item)).toList();
+    } else {
+      throw Exception('Failed to load Pokemons');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: FutureBuilder<List<Pokemon>>(
-          future: pokemonsFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return Text("Error: ${snapshot.error}");
-            } else if (snapshot.hasData) {
-              final pokemons = snapshot.data!;
-              return buildPokemonList(pokemons);
-            } else {
-              return const Text("No data available");
-            }
-          },
-        ),
+        child: _isLoading && _pokemons.isEmpty
+            ? const CircularProgressIndicator()
+            : buildPokemonList(),
       ),
     );
   }
 
-  Widget buildPokemonList(List<Pokemon> pokemons) {
+  Widget buildPokemonList() {
     return ListView.builder(
-      itemCount: pokemons.length,
+      controller: _scrollController,
+      itemCount: _pokemons.length + (_isLoading ? 1 : 0),
       itemBuilder: (context, index) {
-        final pokemon = pokemons[index];
+        if (index == _pokemons.length) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        final pokemon = _pokemons[index];
         return InkWell(
           onTap: () {
             Navigator.push(
@@ -86,8 +112,9 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
             child: Row(
               children: [
                 Image(
-                    image: NetworkImage(
-                        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon.url!.split('/')[6]}.png')),
+                  image: NetworkImage(
+                      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${pokemon.url!.split('/')[6]}.png'),
+                ),
                 Expanded(child: Text('${pokemon.name}')),
               ],
             ),
